d7c74bf182af0574393c9237082036bf5955f05b
B238998-2023 <s2569971@ed.ac.uk>
2023-10-23 00:20:15 +0100

    Script4, heregit add script4.sh

--- script4.sh
+++ script4.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/bash
+
+#Define a "bed_file" variable and assign a value that is the path to a BED file
 bed_file="Tcongo_genome_bed/TriTrypDB-46_TcongolenseIL3000_2019.bed"
 
-# Create a directory to store the counts results
+#Create a directory to store the counts results
 rm -rf couts_data
 mkdir -p counts_data
 
-# Loop through all your sorted BAM files
+#Loop through all "sorted.bam" files
 for bam_file in output2/*_sorted.bam; do
-    # Extract the sample name from the BAM file name (assuming the file names are in a specific format)
+    #Extract the base filename of the current BAM file
     sample_name=$(basename $bam_file)
+    #Change the filename from "_sorted.bam" to "_counts.txt"
     counts_name=${sample_name/_sorted.bam/_counts.txt}
-    # Run bedtools coverage for each BAM file
+    #Use bedtools coverage statistics for each BAM file and store in the "counts_data" directory using countsname
     bedtools coverage -a "$bed_file" -b "$bam_file" > "counts_data/${counts_name}"
 done
