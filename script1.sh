f3f6a713d22758fff584b6b1d3ade6d2d1d7ea6e
B238998-2023 <s2569971@ed.ac.uk>
2023-10-23 00:17:23 +0100

    Here is my first script!

--- script1.sh
+++ script1.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/bash
 
-
-
+#Remove the "fastq" directory and its contents
 rm -rf fastq
+#Copy files from a source directory to a destination directory
 cp -r  /localdisk/data/BPSM/ICA1/fastq ~/ICA1 .
+#Remove and recreate a file named fastq_check
 rm -rf fastq_check
 mkdir -p fastq_check
 
+#Loop though "fq.gz" files to run FastQC
 for file in fastq/*fq.gz ; do
+	#Output files into the "fastq_check" directory
 	fastqc $file -o fastq_check
 done
 
+#When done show the "Successful!" in the screen
 echo "Successful!"
